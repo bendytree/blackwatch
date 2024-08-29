1dda9646f5780193436c7d3e31b945fe5c9581f2
Josh Wright <352285+bendytree@users.noreply.github.com>
2024-08-29 16:35:22 -0500

    fml

--- plugin/source/MainGui.cpp
+++ plugin/source/MainGui.cpp
@@ -13,22 +13,22 @@ MainGui::MainGui(MainAudio& p) : juce::AudioProcessorEditor (&p),
 
   juce::ignoreUnused(mainAudio);
 
-  addAndMakeVisible(*webBrowser);
-
-  // Set the initial size of the editor
-  setSize(1000, 500);
-
-
   // Allow the component to be resized
   setResizable(true, true);
 
   // Set the resize limits to maintain a 2:1 aspect ratio
-  setResizeLimits(400, 200, 2000, 1000);
+  setResizeLimits(400, 200, 1000, 500);
+
+  // Set the initial size of the editor
+  setSize(1000, 500);
 
   // Create and set a ComponentBoundsConstrainer to enforce the aspect ratio
   constrainer = std::make_unique<juce::ComponentBoundsConstrainer>();
   constrainer->setFixedAspectRatio(2.0);
   setConstrainer(constrainer.get());
+
+  addAndMakeVisible(*webBrowser);
+
 }
 
 MainGui::~MainGui()
